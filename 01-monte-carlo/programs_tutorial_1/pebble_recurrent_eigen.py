84ab0086242077770dc7463e2c1acfb0a1341350
supatomic <kngoyal.cs@gmail.com>
2021-05-26 16:02:42 -0400

    print eigenvalues and eigenvectors

--- 01-monte-carlo/programs_tutorial_1/pebble_recurrent_eigen.py
+++ 01-monte-carlo/programs_tutorial_1/pebble_recurrent_eigen.py
@@ -1,14 +1,14 @@
 import numpy
 
 epsilon = 0.01
-transfer = [[ epsilon, 1.0 - epsilon ],
-            [ 1.0 - epsilon, epsilon ]]
+transfer = [[epsilon, 1.0 - epsilon], [1.0 - epsilon, epsilon]]
 eigenvalues, eigenvectors = numpy.linalg.eig(transfer)
-print eigenvalues
- 
-# you may print the eigenvectors by uncommenting the following lines...
-#for iter in range(2):
-#    print eigenvalues[iter]
-#    for i in range(2):
-#       print eigenvectors[i][iter]
+print(eigenvalues)
 
+# you may print the eigenvectors by uncommenting the following lines...
+for iter in range(2):
+    print(eigenvalues[iter])
+    eigenvector = []
+    for i in range(2):
+        eigenvector.append(eigenvectors[i][iter])
+    print(eigenvector)
